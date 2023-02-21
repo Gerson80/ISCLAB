54304d0afd0fbd11200f113449513f7b991991e1
Gerson Herrera <gerson.19070034@gmail.edu.mx>
2023-02-21 16:16:40 -0600

    Primera version terminada

--- src/components/Proyectos.vue
+++ src/components/Proyectos.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="max-w-md overflow-hidden shadow-lg" name="form1">
-        <div class="px-6 py-4 text-white text-xl bg-gradient-to-r from-indigo-500 to-purple-500 rounded-3xl ">
+
+    <div class="grid grid-cols-2 gap-6 py-14 px-36">
+        <div class="px-6 py-4 text-white text-xl bg-gradient-to-r from-indigo-500 to-purple-500 rounded-3xl shadow-lg">
             <h2 class="font-bold text-2xl mb-2">Sitio web</h2>
             <p>Los sitios web son lo que conocemos comúnmente por "página web".
 
@@ -13,7 +14,7 @@
         </div>
 
 
-        <div class="px-6 py-4 text-white text-xl bg-gradient-to-r from-indigo-500 to-purple-500 rounded-3xl ">
+        <div class="px-6 py-4 text-white text-xl bg-gradient-to-r from-indigo-500 to-purple-500 rounded-3xl shadow-lg">
             <h2 class="font-bold text-2xl mb-2">Aplicación web</h2>
 
             <p>Son como los sitios web, pero estos además pueden manipular datos.
@@ -24,7 +25,7 @@
                 mostrar información que cambia con el tiempo
                 realizar cálculos con información de los usuarios</p>
         </div>
-        
+
     </div>
     
     
